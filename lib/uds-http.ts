b835cbdbe1131f785f1c76e6d7d034dabb9e5e46
Daniel Grießhaber <griesshaber@hdm-stuttgart.de>
2023-09-18 09:17:16 +0200

    uds-http: close the underlying unix socket after the stream is read to completion to avoid resource leaks

--- lib/uds-http.ts
+++ lib/uds-http.ts
@@ -22,6 +22,23 @@ function reset_stream(input: ReadableStreamDefaultReader<Uint8Array>): ReadableS
 }
 
 /**
+ * a `BufReader` that closes the underlying connection to the usix socket
+ * after the buffer is read to EOF (null)
+ */
+class AutoClosingReader extends stdio.BufReader {
+    constructor(private connection: Deno.UnixConn) {
+        super(connection);
+    }
+    async read(p: Uint8Array): Promise<number | null> {
+        const result = await super.read(p);
+        if (result === null) {
+            this.connection.close();
+        }
+        return result;
+    }
+}
+
+/**
  * Represents a HTTP connection to a server over a (local) Unix Socket (AF_UNIX).
  * the `fetch` method represents a interface inspired by the `fetch` method in the WebPlatform
  */
@@ -74,7 +91,7 @@ export class UnixHttpSocket {
     }
 
     private async read_response(connection: Deno.UnixConn): Promise<Response> {
-        const http_stream = stdstreams.readableStreamFromReader(new stdio.BufReader(connection));
+        const http_stream = stdstreams.readableStreamFromReader(new AutoClosingReader(connection));
         const line_stream = http_stream.pipeThrough(new stdstreams.DelimiterStream(encoded_crlf, { disposition: "suffix" }));
         let status = -1, status_text = "";
         const headers = new Headers();
@@ -109,6 +126,7 @@ export class UnixHttpSocket {
         if (headers.get("Transfer-Encoding") === "chunked") {
             response_stream = response_stream.pipeThrough(new DechunkingTransferStream());
         }
+
         return new Response(response_stream, { status, statusText: status_text, headers });
     }
 }
