3ac4d34272f0a00bf3407a6e946522e5f8067134
Daniel Grießhaber <griesshaber@hdm-stuttgart.de>
2023-09-21 06:58:38 +0200

    env: add method to get arrays from colon separated strings

--- deps.ts
+++ deps.ts
@@ -1,3 +1,11 @@
 export * as io from "https://deno.land/std@0.201.0/io/mod.ts";
 export * as streams from "https://deno.land/std@0.201.0/streams/mod.ts";
 export * as log from "https://deno.land/std@0.201.0/log/mod.ts";
+export * as path from "https://deno.land/std@0.201.0/path/mod.ts";
+
+import * as front_matter from "https://deno.land/std@0.201.0/front_matter/mod.ts";
+export const extract_frontmatter = front_matter.createExtractor({ [front_matter.Format.JSON]: JSON.parse as front_matter.Parser });
+
+export { SmtpClient } from "https://deno.land/x/smtp@v0.7.0/mod.ts";
+export { Eta } from "https://deno.land/x/eta@v3.1.0/src/index.ts";
+export { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
--- lib/env.ts
+++ lib/env.ts
@@ -1,3 +1,8 @@
+/**
+ * Convenience functions to get values from the environment in the requestst type
+ * with a defaultvalue as fallback
+ */
+
 const env = Deno.env;
 
 export function ensure_defined(...keys: string[]): boolean {
@@ -14,4 +19,17 @@ export function get_string<D extends (string | undefined)>(key: string, default_
 
 export function get_bool<D extends (boolean | undefined)>(key: string, default_value?: D): boolean | D {
     return env.has(key) ? !!env.get(key)! : default_value!;
+}
+
+/**
+ * get a array of values split at `:`
+ * e.g. EXAMPLE=test1:test2::last
+ * ```js
+ * get_array("EXAMPLE")
+ * ["test1", "test2", "", "last"]
+ * ```
+ */
+export function get_array(key: string): string[] {
+    const string_value = env.has(key) ? env.get(key)! : "";
+    return string_value.split(":");
 }
