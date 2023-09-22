626677d71fe9fa1ebf3b74bbc2fd83cd3b4e5c8f
Daniel Grießhaber <griesshaber@hdm-stuttgart.de>
2023-09-22 10:20:29 +0200

    meta: update dependencies to newest stdlib version

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
-export * as io from "https://deno.land/std@0.201.0/io/mod.ts";
-export * as streams from "https://deno.land/std@0.201.0/streams/mod.ts";
-export * as log from "https://deno.land/std@0.201.0/log/mod.ts";
-export * as path from "https://deno.land/std@0.201.0/path/mod.ts";
+export * as io from "https://deno.land/std@0.202.0/io/mod.ts";
+export * as streams from "https://deno.land/std@0.202.0/streams/mod.ts";
+export * as log from "https://deno.land/std@0.202.0/log/mod.ts";
+export * as path from "https://deno.land/std@0.202.0/path/mod.ts";
 
-import * as front_matter from "https://deno.land/std@0.201.0/front_matter/mod.ts";
+import * as front_matter from "https://deno.land/std@0.202.0/front_matter/mod.ts";
 export const extract_frontmatter = front_matter.createExtractor({ [front_matter.Format.JSON]: JSON.parse as front_matter.Parser });
 
 export { SmtpClient } from "https://deno.land/x/smtp@v0.7.0/mod.ts";
