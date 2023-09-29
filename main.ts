0e3c228f52601ec9013c0f78517a07cb954119f0
Dolce Version Updater <dangrie158@users.noreply.github.com>
2023-09-29 08:08:14 +0000

    chore: bump to v2.0.0

--- main.ts
+++ main.ts
@@ -36,7 +36,7 @@ log.setup({
     },
 });
 const logger = log.getLogger("main");
-logger.info(`starting dolce container monitor v1.0.0`);
+logger.info(`starting dolce container monitor v2.0.0`);
 
 // create and register the lockfile, also check if we are already running or experienced an unexpected shutdown
 let restart_time: Date | undefined;
